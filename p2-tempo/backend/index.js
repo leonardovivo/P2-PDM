fa6d3201409b2da3ab6a459c3ea53d70f2cb1a88
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-06-11 18:36:35 -0300

    feat(p2): cria endpoint no backend

--- p2-tempo/backend/index.js
+++ p2-tempo/backend/index.js
@@ -1,28 +1,42 @@
-require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config()
+const axios = require('axios')
+const express = require('express')
+const app = express()
 
 const apiKey = process.env.API_KEY
-const nomeCidade = 'São Paulo';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&appid=${apiKey}&units=metric&lang=pt_br`
+
+app.get('/busca', async (req, res) => {
+  const cidade = req.query.query
 
 
-consulta = async () => {
-    try{
-const resposta = await axios.get(url);
+  try {
+    const resposta = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        q: cidade,
+        appid: apiKey,
+        units: 'metric',
+        lang: 'pt_br'
+      }
+    })
 
-const resultado = {
-    temperatura_minima: resposta.data.main.temp_min,
-    temperatura_maxima: resposta.data.main.temp_max,
-    umidade: resposta.data.main.humidity,
-    nome_icone: resposta.data.weather[0].icon,
-    descricao: resposta.data.weather[0].description
-}
+    const resultado = {
+      temperatura_minima: resposta.data.main.temp_min,
+      temperatura_maxima: resposta.data.main.temp_max,
+      umidade: resposta.data.main.humidity,
+      nome_icone: resposta.data.weather[0].icon,
+      descricao: resposta.data.weather[0].description
+    }
 
-console.log(resultado);
+    console.log(`Cidade buscada: ${cidade}`)
+    console.log(resultado)
 
-} catch(error){
-    console.log('Houve um erro!', error);
-}
-}
+    res.json(resultado)
 
-consulta();
+  } catch (error) {
+    console.error('Erro na consulta:', error.message)
+    res.status(500).json({ erro: 'Erro na comunicação com a API da OpenWeatherMap' })
+  }
+})
+
+const port = 5002
+app.listen(port, () => console.log(`Backend funcionando. Porta: ${port}`))

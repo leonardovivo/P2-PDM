e485cd73e71dd417b00a1b46b3596f74d1e99ae0
Leonardo Vivo Guerreiro <leonardovivo3@gmail.com>
2025-06-12 16:17:24 -0300

    feat(p2): Protege contra ataques XSS

--- p2-tempo/src/components/Busca.jsx
+++ p2-tempo/src/components/Busca.jsx
@@ -1,35 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import striptags from 'striptags'
 
 const Busca = () => {
   const [termoDeBusca, setTermoDeBusca] = useState('São Paulo')
 
   useEffect(() => {
-    if (termoDeBusca.length < 3) {
+    if (termoDeBusca.length < 3) 
       return
-    }
+    
 
     const fazerBusca = async () => {
       try {
         const resposta = await axios.get('http://localhost:5002/busca', {
           params: { query: termoDeBusca }
         })
-        console.log(resposta.data)
+
+
+        const respostaLimpa = [{
+          temperatura_minima: striptags(resposta.data.temperatura_minima.toString()),
+          temperatura_maxima: striptags(resposta.data.temperatura_maxima.toString()),
+          umidade: striptags(resposta.data.umidade.toString()),
+          nome_icone: striptags(resposta.data.nome_icone),
+          descricao: striptags(resposta.data.descricao)
+        }]
+
+        setDados(respostaLimpa)
+
       } catch (error) {
         console.error('Erro:', error)
+        setDados([])
       }
     }
 
     const timeoutID = setTimeout(() => {
       fazerBusca()
     }, 2000)
-    return () => {
-      clearTimeout(timeoutID)
-    }
+
+    return () => clearTimeout(timeoutID)
   }, [termoDeBusca])
 
   return (
-    <div>
+    <div className="flex flex-column align-items-center">
       <input
         type="text"
         value={termoDeBusca}
